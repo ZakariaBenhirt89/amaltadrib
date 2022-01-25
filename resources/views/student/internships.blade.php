0f208674b447ee8ac3f815ebf4026949710b71af
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2022-01-25 15:46:15 +0100

    student all jobs & all internships

--- resources/views/student/internships.blade.php
+++ resources/views/student/internships.blade.php
@@ -13,58 +13,28 @@
                     <div class="table-responsive">
                         <table class="table table-striped table-bordered" id="table">
                             <thead>
+                                <th>من</th>
+                                <th>إلى</th>
+                                <th>إسم المقر</th>
                                 <th>العنوان</th>
-                                <th>المزود</th>
-                                <th>تبدأ في</th>
-                                <th>ينتهي عند</th>
-                                <th>المشرف</th>
-                                <th>البريد الإلكتروني للمشرف</th>
-                                <th>هاتف المشرف</th>
-                                <th>الأهداف</th>
-                                <th>إرشادات</th>
-                                <th></th>
                             </thead>
                             <tbody>
                                 @isset($internships)
-                                    @foreach ($internships as $internship)
-                                        <tr>
-                                            <td>{{ $internship->title }}</td>
-                                            <td>{{ $internship->salary }}</td>
-                                            <td>{{ $internship->position }}</td>
-                                            <td>{{ $internship->provider }}</td>
-                                            <td>{{ $internship->location }}</td>
-                                            <td>{{ $internship->supervisor }}</td>
-                                            <td>{{ $internship->supervisor_email }}</td>
-                                            <td>{{ $internship->supervisor_phone }}</td>
-                                            <td>{{ $internship->start }}</td>
-                                            <td>{{ $internship->end }}</td>
-                                            <td>{{ $internship->contract_type }}</td>
-                                            <td>{{ $internship->description }}</td>
-                                            <td>
-                                                @if ($internship->applied == 0)
-                                                    <form action="{{ route('student.internship.apply',$internship->id) }}" method="post">
-                                                        @csrf
-                                                        <button type="submit" class="btn btn-sm btn-success text-white">تقديم طلب</button>
-                                                    </form>
-                                                @else
-                                                <span></span>
-                                                @endif
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                @endisset
+                          @foreach ($internships as $internship)
+                            <tr>
+                              <td class="text-nowrap">{{ $internship->start}}</td>
+                              <td class="text-nowrap">{{ $internship->end}}</td>
+                              <td class="text-nowrap">{{ $internship->provider}}</td>
+                              <td class="text-nowrap">{{ $internship->address}}</td>
+                            </tr>
+                          @endforeach
+                        @endisset
                             </tbody>
                             <tfoot>
+                                <th>من</th>
+                                <th>إلى</th>
+                                <th>إسم المقر</th>
                                 <th>العنوان</th>
-                                <th>المزود</th>
-                                <th>تبدأ في</th>
-                                <th>ينتهي عند</th>
-                                <th>المشرف</th>
-                                <th>البريد الإلكتروني للمشرف</th>
-                                <th>هاتف المشرف</th>
-                                <th>الأهداف</th>
-                                <th>إرشادات</th>
-                                <th></th>
                             </tfoot>
                         </table>
                     </div>
--- resources/views/student/job.blade.php
+++ resources/views/student/job.blade.php
@@ -13,64 +13,28 @@
                     <div class="table-responsive">
                         <table class="table table-striped table-bordered" id="table">
                             <thead>
-                                <th>العنوان</th>
-                                <th>الراتب</th>
-                                <th>المنصب</th>
-                                <th>المزود</th>
-                                <th>الموقع</th>
-                                <th>المشرف</th>
-                                <th>البريد الإلكتروني للمشرف</th>
-                                <th>هاتف المشرف</th>
-                                <th>من</th>
-                                <th>إلى</th>
-                                <th>نوع العقد</th>
-                                <th>الوصف</th>
-                                <th></th>
+                              <th>من</th>
+                              <th>إلى</th>
+                              <th>إسم المقر</th>
+                              <th>العنوان</th>
                             </thead>
                             <tbody>
-                                @isset($jobs)
-                                    @foreach ($jobs as $job)
-                                        <tr>
-                                            <td>{{ $job->title }}</td>
-                                            <td>{{ $job->salary }}</td>
-                                            <td>{{ $job->position }}</td>
-                                            <td>{{ $job->provider }}</td>
-                                            <td>{{ $job->location }}</td>
-                                            <td>{{ $job->supervisor }}</td>
-                                            <td>{{ $job->supervisor_email }}</td>
-                                            <td>{{ $job->supervisor_phone }}</td>
-                                            <td>{{ $job->start }}</td>
-                                            <td>{{ $job->end }}</td>
-                                            <td>{{ $job->contract_type }}</td>
-                                            <td>{{ $job->description }}</td>
-                                            <td>
-                                                @if ($job->applied == 0)
-                                                    <form action="{{ route('student.job.apply',$job->id) }}" method="post">
-                                                        @csrf
-                                                        <button type="submit" class="btn btn-sm btn-success text-white">تقديم طلب</button>
-                                                    </form>
-                                                @else
-                                                <span></span>
-                                                @endif
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                @endisset
+                              @isset($jobs)
+                                @foreach ($jobs as $job)
+                                  <tr>
+                                    <td class="text-nowrap">{{ $job->start}}</td>
+                                    <td class="text-nowrap">{{ $job->end}}</td>
+                                    <td class="text-nowrap">{{ $job->provider}}</td>
+                                    <td class="text-nowrap">{{ $job->address}}</td>
+                                  </tr>
+                                @endforeach
+                              @endisset
                             </tbody>
                             <tfoot>
-                                <th>العنوان</th>
-                                <th>الراتب</th>
-                                <th>المنصب</th>
-                                <th>المزود</th>
-                                <th>الموقع</th>
-                                <th>المشرف</th>
-                                <th>البريد الإلكتروني للمشرف</th>
-                                <th>هاتف المشرف</th>
-                                <th>من</th>
-                                <th>إلى</th>
-                                <th>نوع العقد</th>
-                                <th>الوصف</th>
-                                <th></th>
+                              <th>من</th>
+                              <th>إلى</th>
+                              <th>إسم المقر</th>
+                              <th>العنوان</th>
                             </tfoot>
                         </table>
                     </div>
