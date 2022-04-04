17c62c20275042f8d1c2b1d5cabab1702177ce64
Abdelaziz Bardich <76938240+abdelazizbardich@users.noreply.github.com>
2022-04-04 14:10:44 +0000

    updating file name & removing link from home podcasts

--- resources/views/student/home.blade.php
+++ resources/views/student/home.blade.php
@@ -65,7 +65,7 @@
                 @foreach ($videos as $video)
                     <div class="col-md-4 mb-3">
                         <div class="shadow-md border border-warning rounded p-1 bg-warning">
-                            <a @if($video->watched) href="{{ route("student.video",$video->id)}}" @endif>
+                            <a @if($video->watched)  @endif>
                                 <div class="video-box @if(!$video->watched) disabled @endif rounded">
                                     <img src="{{route("resources.video.thumbnail",$video->thumbnail)}}" alt="{{ $video->title }}" class="img-responsive img w-100">
                                     <div class="details">
